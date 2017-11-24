4de3a5b442bd3250d4aedb3408a7c0dad94e1b9b
Ryno Beyers <rynobey@gmail.com>
2017-11-24 14:31:49 +0200

    Stats collected from QuorumNetworkProbe now appended to file. File starts with a header line to label data columns.

--- log.js
+++ log.js
@@ -84,11 +84,130 @@ function log() {
     });
   }
 
+  function appendCPUStats(logObj) {
+    let timestamp;
+    if (logObj.timestamp) {
+      timestamp = logObj.timestamp;
+    } else {
+      appendError({
+        msg: 'ERROR in log.appendCPUStats: logObj contains no timestamp!'
+      });
+    }
+    let filePath = object.path + '/hostCPUStats.log';
+    let str = timestamp + ',' + logObj.numCpus + ',' + logObj.utilization + ',' +
+      logObj.loadAvg1m + ',' + logObj.loadAvg5m + ',' + logObj.loadAvg15m + '\n';
+    fs.open(filePath, 'ax', function(err, fd) {
+      if (err && err.code == 'EEXIST') {
+        fs.open(filePath, 'a', function(err, fd) {
+          fs.write(fd, str, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendCPUStats: ' + err
+              });
+            }
+          });
+        });
+      } else { // file does not exist yet, write header line first
+        fs.open(filePath, 'a', function(err, fd) {
+          let headerStr = 'timestamp,numCpus,utilization,loadAvg1m,loadAvg5m,loadAvg15m\n';
+          let totalStr = headerStr + str;
+          fs.write(fd, totalStr, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendCPUStats: ' + err
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
+  function appendMemStats(logObj) {
+    let timestamp;
+    if (logObj.timestamp) {
+      timestamp = logObj.timestamp;
+    } else {
+      appendError({
+        msg: 'ERROR in log.appendMemStats: logObj contains no timestamp!'
+      });
+    }
+    let filePath = object.path + '/hostMemStats.log';
+    let str = timestamp + ',' + logObj.kBAvailable + ',' + logObj.kBTotal + '\n';
+    fs.open(filePath, 'ax', function(err, fd) {
+      if (err && err.code == 'EEXIST') {
+        fs.open(filePath, 'a', function(err, fd) {
+          fs.write(fd, str, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendMemStats: ' + err
+              });
+            }
+          });
+        });
+      } else { // file does not exist yet, write header line first
+        fs.open(filePath, 'a', function(err, fd) {
+          let headerStr = 'timestamp,kBAvailable,kBTotal\n';
+          let totalStr = headerStr + str;
+          fs.write(fd, totalStr, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendMemStats: ' + err
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
+  function appendDiskStats(logObj) {
+    let timestamp;
+    if (logObj.timestamp) {
+      timestamp = logObj.timestamp;
+    } else {
+      appendError({
+        msg: 'ERROR in log.appendDiskStats: logObj contains no timestamp!'
+      });
+    }
+    let filePath = object.path + '/hostDiskStats.log';
+    let str = timestamp + ',' + logObj.iowait + ',' + logObj.await + ',' +
+      logObj.svctm + ',' + logObj.kBpsRead + ',' + logObj.kBpsWrite + '\n';
+    fs.open(filePath, 'ax', function(err, fd) {
+      if (err && err.code == 'EEXIST') {
+        fs.open(filePath, 'a', function(err, fd) {
+          fs.write(fd, str, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendDiskStats: ' + err
+              });
+            }
+          });
+        });
+      } else { // file does not exist yet, write header line first
+        fs.open(filePath, 'a', function(err, fd) {
+          let headerStr = 'timestamp,iowait,await,svctm,kBpsRead,kBpsWrite\n';
+          let totalStr = headerStr + str;
+          fs.write(fd, totalStr, function(err, wrtn, str) {
+            if (err) {
+              appendError({
+                msg: 'ERROR in log.appendDiskStats: ' + err
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
   object.Initialize = initialize;
   object.AppendTxHashRequest = appendTxHashRequest;
   object.AppendTxHashResponse = appendTxHashResponse;
   object.AppendStatusUpdate = appendStatusUpdate;
   object.AppendError = appendError;
+  object.AppendCPUStats = appendCPUStats;
+  object.AppendMemStats = appendMemStats;
+  object.AppendDiskStats = appendDiskStats;
   return object;
 }
 
