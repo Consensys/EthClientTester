ead7094526a5f0e97c69295655e74176899e83d0
Ryno Beyers <rynobey@gmail.com>
2017-12-08 16:13:24 +0200

    Increased the total supply of ERC20 tokens

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,7 +4,7 @@ import "./contracts/Tokens/contracts/StandardToken.sol";
 
 contract ERC20 is StandardToken{
   
-  uint256 public totalSupply = 100;
+  uint256 public totalSupply = 100000000;
  
   function ERC20() {
     balances[msg.sender] = totalSupply; 
