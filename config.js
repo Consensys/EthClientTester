390eda078d6305ae795e761e88707f932e2f6624
Ryno Beyers <rynobey@gmail.com>
2017-12-21 09:33:59 +0200

    Reverted config.js back to default

--- config.js
+++ config.js
@@ -2,37 +2,47 @@ var config = {}
 
 /*  Connection settings
 */
-config.clientType = 'go-quorum';
+config.clientType = 'ethereumjs-testrpc';
 config.nodes = [
   {
-    name: "node1",
-    web3RPCHost: "51.140.42.117",
-    web3RPCPort: "20010",
+    name: 'testrpc1',
+    web3RPCHost: '127.0.0.1',
+    web3RPCPort: '8545',
     genTraffic: true
-  }, {
-    name: "node2",
-    web3RPCHost: "51.140.176.110",
-    web3RPCPort: "20010",
-    genTraffic: false
-  }, {
-    name: "node3",
-    web3RPCHost: "51.140.180.108",
-    web3RPCPort: "20010",
-    genTraffic: false
-  }, {
-    name: "node4",
-    web3RPCHost: "51.140.161.117",
-    web3RPCPort: "20010",
-    genTraffic: false
   }
 ];
+
+//config.clientType = 'go-quorum';
+//config.nodes = [
+//  {
+//    name: "node1",
+//    web3RPCHost: "51.140.42.117",
+//    web3RPCPort: "20010",
+//    genTraffic: true
+//  }, {
+//    name: "node2",
+//    web3RPCHost: "51.140.176.110",
+//    web3RPCPort: "20010",
+//    genTraffic: false
+//  }, {
+//    name: "node3",
+//    web3RPCHost: "51.140.180.108",
+//    web3RPCPort: "20010",
+//    genTraffic: false
+//  }, {
+//    name: "node4",
+//    web3RPCHost: "51.140.161.117",
+//    web3RPCPort: "20010",
+//    genTraffic: false
+//  }
+//];
 
 /*  Test settings
 */
 config.tests = [
-  'tokenTransferRampedExample1.js'
+  //'tokenTransferRampedExample1.js'
   //'fetchBlocks.js'
-  //'etherTransactionExample1.js'
+  'etherTransactionExample1.js'
   //'tokenContractAsynchronuousExample1.js'
   //'tokenTransferConstantRate1.js'
 ];
