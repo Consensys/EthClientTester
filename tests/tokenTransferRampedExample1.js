83a864a91b25fffb4e383d887f0e32ad8c448108
Ryno Beyers <rynobey@gmail.com>
2018-01-17 00:45:41 +0200

    Updated a test scenario

--- tests/tokenTransferRampedExample1.js
+++ tests/tokenTransferRampedExample1.js
@@ -1,9 +1,10 @@
 var scheduler = require('../scheduler.js');
 var ERC20 = require('../contracts/ERC20.js');
 
-var outerIterations = 10
+var outerIterations = 20
 var outerFrequency = 1/30
 var blockFetchFrequency = 50
+var numBatchTx = 10
 
 module.exports.prepare = function(seq) {
   seq.push(function(result, cb) {
@@ -27,15 +28,9 @@ module.exports.prepare = function(seq) {
   seq.push(function(result, cb) {
     let contracts = result.contracts;
     let accounts = result.accounts;
-    contracts.Deployed[0].transfer(accounts.Unlocked[1], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[2], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[3], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[4], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[5], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[6], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[7], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[8], 10000000, {from: accounts.Unlocked[0]});
-    contracts.Deployed[0].transfer(accounts.Unlocked[9], 10000000, {from: accounts.Unlocked[0]});
+    for (let i = 1; i < numBatchTx; i++) {
+      contracts.Deployed[0].transfer(accounts.Unlocked[i], 10000000, {from: accounts.Unlocked[0]});
+    }
     cb(null, result);
   });
   seq.push(function(result, cb) {
@@ -53,7 +48,7 @@ module.exports.execute = function(seq) {
     var alternateCount = 0;
     scheduler.Alternate(function(repeater) { // do tx
       let txFrequency = 5 + 1*alternateCount;
-      let txIterations = Math.floor(((1/outerFrequency)/3)/(1/txFrequency)); // submit tx for 1/5 of the time
+      let txIterations = Math.floor(((1/outerFrequency)/4)/(1/txFrequency)); // submit tx for 1/4 of the time
       scheduler.Repeat(function(repeater1) {
         let transactions = result.transactions;
         let contracts = result.contracts;
@@ -63,36 +58,15 @@ module.exports.execute = function(seq) {
           transactions: []
         };
         // specifying a gas amount avoids an estimate from being done - saves time
-        let tx1 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[0], 1, 
-          {from: accounts.Unlocked[1], gas: 100000});
-        let tx2 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[1], 1, 
-          {from: accounts.Unlocked[2], gas: 100000});
-        let tx3 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[2], 1, 
-          {from: accounts.Unlocked[3], gas: 100000});
-        let tx4 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[3], 1, 
-          {from: accounts.Unlocked[4], gas: 100000});
-        let tx5 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[4], 1, 
-          {from: accounts.Unlocked[5], gas: 100000});
-        let tx6 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[5], 1, 
-          {from: accounts.Unlocked[6], gas: 100000});
-        let tx7 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[6], 1, 
-          {from: accounts.Unlocked[7], gas: 100000});
-        let tx8 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[7], 1, 
-          {from: accounts.Unlocked[8], gas: 100000});
-        let tx9 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[8], 1, 
-          {from: accounts.Unlocked[9], gas: 100000});
-        let tx10 = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[9], 1, 
-          {from: accounts.Unlocked[0], gas: 100000});
-        result.txOptions.transactions.push(tx1);
-        result.txOptions.transactions.push(tx2);
-        result.txOptions.transactions.push(tx3);
-        result.txOptions.transactions.push(tx4);
-        result.txOptions.transactions.push(tx5);
-        result.txOptions.transactions.push(tx6);
-        result.txOptions.transactions.push(tx7);
-        result.txOptions.transactions.push(tx8);
-        result.txOptions.transactions.push(tx9);
-        result.txOptions.transactions.push(tx10);
+        let tx = [];
+        for (let i = 0; i < numBatchTx; i++) {
+          tx[i] = contracts.Deployed[0].transfer.getTx(accounts.Unlocked[i], 1, 
+          {from: accounts.Unlocked[(i+1)%numBatchTx], gas: 100000});
+        }
+        //  {from: accounts.Unlocked[0], gas: 100000});
+        for (let i = 0; i < numBatchTx; i++) {
+          result.txOptions.transactions.push(tx[i]);
+        }
         transactions.SendBatch(result); // no cb passed to indicate that called from within repeater
       }, txIterations, txFrequency, function() {
         repeater.completed();
