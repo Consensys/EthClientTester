8e0e1230965f3dfac2fb2274fd84dfa80b30501e
Ryno Beyers <rynobey@gmail.com>
2018-04-08 21:27:50 +0200

    Changed config defaults

--- config.js
+++ config.js
@@ -16,22 +16,22 @@ config.nodes = [
 //config.nodes = [
 //  {
 //    name: "node1",
-//    web3RPCHost: "51.140.42.117",
+//    web3RPCHost: "127.0.0.1",
 //    web3RPCPort: "20010",
 //    genTraffic: true
 //  }, {
 //    name: "node2",
-//    web3RPCHost: "51.140.176.110",
+//    web3RPCHost: "192.168.0.2",
 //    web3RPCPort: "20010",
 //    genTraffic: false
 //  }, {
 //    name: "node3",
-//    web3RPCHost: "51.140.180.108",
+//    web3RPCHost: "192.168.0.3",
 //    web3RPCPort: "20010",
 //    genTraffic: false
 //  }, {
 //    name: "node4",
-//    web3RPCHost: "51.140.161.117",
+//    web3RPCHost: "192.168.0.4",
 //    web3RPCPort: "20010",
 //    genTraffic: false
 //  }
@@ -40,11 +40,8 @@ config.nodes = [
 /*  Test settings
 */
 config.tests = [
-  //'tokenTransferRampedExample1.js'
-  //'fetchBlocks.js'
-  'etherTransactionExample1.js'
-  //'tokenContractAsynchronuousExample1.js'
-  //'tokenTransferConstantRate1.js'
+  'tokenTransferRampedExample1.js'
+  //'etherTransactionExample2.js'
 ];
 
 module.exports = config
